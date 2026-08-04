268b27c7a78fb7296bec93a42e06c27886d8d04e
Peter Bauer <peter.bauer@bajupa.com>
2026-08-04 14:23:13 +0200

    Added assetPath

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -4,3 +4,11 @@ export const baseHref = base.endsWith("/") ? base : `${base}/`;
 
 export const assetPath = (path: string) =>
   `${baseHref}${path.replace(/^\/+/, "")}`;
+
+export const pagePath = (path: string) => {
+  if (path.startsWith(baseHref)) {
+    return path;
+  }
+
+  return `${baseHref}${path.replace(/^\/+/, "")}`;
+};
