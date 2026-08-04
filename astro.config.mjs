ec741a1c84e77289104aac59f6c40349fd402d98
Peter Bauer <peter.bauer@bajupa.com>
2026-08-04 11:37:24 +0200

    Generalized deployment to prepare for staging and release

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,24 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({
-  site: "https://bauepete.github.io",
-  base: "/zengruppe-linz",
-});
+const target = process.env.DEPLOY_TARGET ?? "development";
+
+const targets = {
+  development: {
+    site: "https://zengruppe-linz.github.io",
+    base: "/development",
+  },
+  staging: {
+    site: "https://zengruppe-linz.github.io",
+    base: "/staging",
+  },
+  production: {
+    site: "https://zengruppe-linz.at",
+    base: "/",
+  },
+};
+
+if (!(target in targets)) {
+  throw new Error(`Unknown DEPLOY_TARGET: ${target}`);
+}
+
+export default defineConfig(targets[target]);
